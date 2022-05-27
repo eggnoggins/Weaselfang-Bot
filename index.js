46e93b0b39a5c52755be7f5fb41f79fc00669a77
Milk <mashmallows131@gmail.com>
2022-05-26 21:57:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
+const { prefix } = "w!";
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Weaselfang has been sent to the mountain!');
 });
-
-client.user.setPresence({ activities: [{ name: 'with discord.js' }], status: 'idle' });
 
 // Login to Discord with your client's token
 client.login(token);
