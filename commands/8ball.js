43cec3e9e2f018f706f32c69d562e0798506c523
Milk <mashmallows131@gmail.com>
2022-05-28 02:13:16 -0400

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,14 +1,11 @@
 exports.run = (client, message, args) => {
-    client.on('messageCreate', async message => {
-        if (message.author.bot) return
-        if (message.content.toLowerCase().startsWith("weasel,")) {
-          let replies = ['answer', 'yet another answer', 'wow another answer', 'you guessed it, another answer'];
-          let result = replies[Math.floor(Math.random() * (replies.length))]
-    
-          message.reply(result)
-        }
-      })
-    message.channel.send("pong!").catch(console.error);
+    if (message.author.bot) return
+    if (message.content.toLowerCase().startsWith("komaeda,")) {
+      let replies = ['answer', 'yet another answer', 'wow another answer', 'you guessed it, another answer'];
+      let result = replies[Math.floor(Math.random() * (replies.length))]
+
+      message.reply(result)
+    }
 }
 
   exports.name = "8ball";
