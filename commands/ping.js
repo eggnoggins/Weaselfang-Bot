86219cf9402c5cd91ef05cf850367d0e92dd69b1
Milk <mashmallows131@gmail.com>
2022-05-27 21:20:09 -0400

    yes

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,17 @@
 
+module.exports = {}
+callback: (message, ...args) => {
+    let sum = 0
+
+    for (const arg of args) {
+        sum += parseInt(arg)
+    }
+
+    message.reply(`The sum is ${sum}`)
+},
+
+
+
 client.on("messageCreate", (message) => {
     if (message.content.startsWith("ping")) {
       message.channel.send("pong!");
