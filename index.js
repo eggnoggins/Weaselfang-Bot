0cb33657c0b44f477e35bda8918d7ebad29bc189
Milk <mashmallows131@gmail.com>
2022-05-28 02:29:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,18 @@ client.once('ready', () => {
 client.on('messageCreate', async message => {
     if (message.author.bot) return
     if (message.content.toLowerCase().startsWith("weasel,")) {
-      let replies = ['answer', 'yet another answer', 'wow another answer', 'you guessed it, another answer'];
+      let replies = 
+	  ["Yes.", 
+	  "No.", 
+	  "It really depends.", 
+	  "I wouldn't think so.",
+	  "As if it ever could.",
+	  "You shouldn't test your luck.",
+	  "Very well.",
+	  "If it makes you happy, yes.",
+	  "I don't see why not.",
+	  "Only StarClan knows.",
+	  "''Weaselfang lets out a yawn, blinking at you.'' I'm a bit busy right now, ask someone else.",];
       let result = replies[Math.floor(Math.random() * (replies.length))]
 
       message.reply(result)
