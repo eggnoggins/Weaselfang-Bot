d84e177b36b79e189828c3794e5088237b439117
Milk <mashmallows131@gmail.com>
2022-05-27 23:17:58 -0400

    ong

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // Require the necessary discord.js classes
 const DJS = require('discord.js')
 const { Intents } = DJS
-require('dotenv/config')
+const { token } = require('./config.json');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 // Create a new client instance
-const client = new Client({ 
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+const client = new DiscordJS.Client({ 
+	intents: [
+		Intents.FLAGS.GUILDS, 
+		Intents.FLAGS.GUILD_MESSAGES
+	]
  });
 
 // When the client is ready, run this code (only once)
@@ -14,9 +17,9 @@ client.on('ready', () => {
 	console.log('Weaselfang has been sent to the mountain!')
 	let handler = require('./command-handler')
 	if (handler.default) handler = handler.default
-	client.user.setPresence({ activities: [{ name: 'with fire 🔥' }], type: 'PLAYING' });
 
 	handler(client)
 });
 
-client.login(process.env.TOKEN);
+
+client.login(token);
