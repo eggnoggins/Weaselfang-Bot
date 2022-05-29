e7e5f0fdcf8c0b2f0f453cc28a665201009e8614
Milk <mashmallows131@gmail.com>
2022-05-28 20:56:39 -0400

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,6 +1,7 @@
 exports.run = (client, message, args) => {
         if (message.author.bot) return
       let replies = [
+        message.channel.send('<@' + message.author.id + '> pong'),
         '🎲 <@${user.id}> rolled a 1!',
         '🎲 <@${user.id}> rolled a 2!', 
         '🎲 <@${user.id}> rolled a 3!', 
