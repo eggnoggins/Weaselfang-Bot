b937b5174de3e4cbda4d86c17efe7974e4d5e09e
Milk <mashmallows131@gmail.com>
2022-05-29 14:22:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new Client({
 	const commands = fs.readdirSync("./commands/${folder}").filter(file => file.endsWith(".js"));
     for (const file of commands) {
       const commandName = file.split(".")[0];
-      const command = requirwe(`./commands/${folder}/${file}`);
+      const command = require(`./commands/${folder}/${file}`);
   
     console.log(`Attempting to load command ${commandName}`);
     client.commands.set(commandName, command);
