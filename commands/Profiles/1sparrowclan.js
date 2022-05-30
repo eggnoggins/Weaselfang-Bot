368584d3875f8a618bdbdddf8a52c047b6159305
Milk <mashmallows131@gmail.com>
2022-05-30 04:09:14 -0400

    Update 1sparrowclan.js

--- commands/Profiles/1sparrowclan.js
+++ commands/Profiles/1sparrowclan.js
@@ -6,6 +6,9 @@ module.exports = {
     cooldown: "1s",
     callback: async ({ message, args, client }) => {
 message.reply(`
+<:female:980152668020412467> __**Saltstar**__
+<:female:980152668020412467> __**Saltstar**__
+<:female:980152668020412467> __**Saltstar**__
 <:female:980152668020412467> __**Saltstar**__`)
 
 }}
