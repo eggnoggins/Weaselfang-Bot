23acff225d852b57de2ac97fb40bcf7c1766a5af
Milk <mashmallows131@gmail.com>
2022-05-30 04:07:47 -0400

    ss

---
+++ commands/Profiles/1sparrowclan.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "saltstar",
+    description: "desc",
+    category: "basic",
+    slash: false,
+    cooldown: "1s",
+    callback: async ({ message, args, client }) => {
+message.reply(`
+<:female:980152668020412467> __**Batstar**__`)
+
+}}
