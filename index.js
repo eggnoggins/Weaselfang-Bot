b2173ba28ae1230abeece3fa762cc438d4c6dc59
Milk <mashmallows131@gmail.com>
2022-05-31 16:58:10 -0400

    augh

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const client = new Client({
 		Intents.FLAGS.GUILD_MESSAGES,
 		Intents.FLAGS.GUILD_MEMBERS]
   });
+  const config = require("./config.json");
+  // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
+  client.config = config;
   client.commands = new Collection();
   
   client.on('ready', async () => {
@@ -20,7 +23,7 @@ const client = new Client({
 		'mongodb+srv://eggnoggins:<password>@weaselfang.sngrcuw.mongodb.net/?retryWrites=true&w=majority',
 		{}
 	)
-
+	
 	new WOKCommands(client, {
 		  commandsDir: path.join(__dirname, 'commands'),
 		  disabledDefaultCommands: [
